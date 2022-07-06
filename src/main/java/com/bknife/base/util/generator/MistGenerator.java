40d81132a9cf5a2c2766450500527d650fa3bf3c
blackknifes <a1991815a@gmail.com>
2022-07-07 00:06:57 +0800

    添加薄雾算法声明

---
+++ src/main/java/com/bknife/base/util/generator/MistGenerator.java
@@ -0,0 +1,8 @@
+package com.bknife.base.util.generator;
+
+public class MistGenerator {
+    private static final long INCRESMENT_MASK = 0x1FFFFFFFFFFl;
+    private static final long INCRESMENT_OFFSET = 2;
+    private static final long SALT_MASK = 0xFF;
+
+}
